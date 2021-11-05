3e396c7b7cae6c92bcbbc24e00656e8397afeeda
Nikita Nitichevski <me@donnikitos.com>
2021-11-05 16:08:09 +0200

    added token auth to api requests

--- src/functions/requestAPI.js
+++ src/functions/requestAPI.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 import { API_URL, API_VERSION } from '@/constants/index.js';
+import store from '@/store/index.js';
 import { useToast } from 'vue-toastification';
 
 const toast = useToast();
 
 async function requestAPI(method, url, args, withVersion = true) {
 	let result = undefined;
+	const token = store.state.auth.token;
+
+	let headers = undefined;
+	if (token) {
+		headers = {
+			// 'Access-Control-Allow-Origin': '*',
+			Authorization: token ? 'Bearer ' + token : undefined,
+		};
+	}
 
 	try {
 		result = await axios({
 			method,
 			url: `${API_URL}${withVersion ? `/${API_VERSION}` : ''}/${url}`,
+			headers,
 			...args,
 		});
 	} catch (e) {
