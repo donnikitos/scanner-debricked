528dc51910c438505d351d4cebc5771d70082252
Nikita Nitichevski <me@donnikitos.com>
2021-11-05 14:36:22 +0200

    created global components

---
+++ src/components/Button/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<button :type="type" :class="$style.root"><slot></slot></button>
+</template>
+
+<script>
+export default {
+	name: 'Input',
+	props: {
+		type: {
+			type: String,
+			default: 'button',
+			validator(prop) {
+				return ['button', 'reset', 'submit'].includes(prop);
+			},
+		},
+	},
+};
+</script>
+
+<style lang="scss" module src="./styles.scss" />
---
+++ src/components/Input/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<input
+		:type="type"
+		:class="$style.root"
+		:placeholder="placeholder"
+		:value="modelValue"
+		@input="$emit('update:modelValue', $event.target.value)"
+	/>
+</template>
+
+<script>
+export default {
+	name: 'Input',
+	props: {
+		type: {
+			type: String,
+			default: 'text',
+			validator(prop) {
+				return ['text', 'password'].includes(prop);
+			},
+		},
+		modelValue: {
+			type: String,
+		},
+		placeholder: {
+			type: String,
+		},
+	},
+	emits: {
+		'update:modelValue': null,
+	},
+};
+</script>
+
+<style lang="scss" module src="./styles.scss" />
