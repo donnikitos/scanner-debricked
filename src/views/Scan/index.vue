3ae296f84fc4229c367850aa75cf5913b0b56128
Nikita Nitichevski <me@donnikitos.com>
2021-11-05 16:11:18 +0200

    added file upload action + file fiever

--- src/views/Scan/index.vue
+++ src/views/Scan/index.vue
@@ -1,23 +1,44 @@
 <template>
 	<div :class="$style.root">
 		<Uploader v-if="!file" @onSelect="onFileSelect" />
+		<FileViewer v-if="file" :data="file.data" />
 	</div>
 </template>
 
 <script>
 import Uploader from './components/Uploader/index.vue';
+import FileViewer from './components/FileViewer/index.vue';
+import requestAPI from '@/functions/requestAPI';
 
 export default {
 	name: 'Scan',
-	components: { Uploader },
+	components: { Uploader, FileViewer },
 	data() {
 		return {
 			file: undefined,
+			uploadInfo: {
+				ciUploadId: undefined,
+				uploadProgramsFileId: undefined,
+			},
 		};
 	},
 	methods: {
-		onFileSelect(payload) {
+		async onFileSelect(payload) {
 			this.file = payload;
+
+			const res = await requestAPI(
+				'POST',
+				'open/uploads/dependencies/files',
+				{
+					repositoryName: 'unknown',
+					commitName: 'unknown',
+					fileData: payload.file,
+				},
+			);
+
+			if (res) {
+				this.uploadInfo = res;
+			}
 		},
 	},
 };
