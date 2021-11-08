74e49d73f8a53629107a4dd7547006ed3184c9f4
Nikita Nitichevski <me@donnikitos.com>
2021-11-08 13:16:25 +0200

    fixed constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
-export const API_URL = ' //localhost:8081/api';
-// export const API_URL = ' https://debricked.com/api';
+export const isDev = process.env.NODE_ENV === 'development';
+
+export const API_URL = isDev
+	? '//localhost:8081/api'
+	: 'https://debricked.com/api';
 
 export const API_VERSION = '1.0';
